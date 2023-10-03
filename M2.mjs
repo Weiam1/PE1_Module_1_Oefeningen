8c0b9876687722a99a07004a8f7689ed7b61430e
Weiam1 <walmahnash@gmail.com>
2023-10-03 16:02:13 +0200

    k

---
+++ M2.mjs
@@ -0,0 +1,7 @@
+let geboorteJaar=2001;
+let toekomstjaar=2031;
+let oud= toekomstjaar-geboorteJaar;
+let ouder =oud +1 ;
+
+
+console.log( 'In '+ toekomstjaar + ' zal ik '  + oud  + ' of ' + ouder  + ' jaar oud zijn');
--- e3_opteller.mjs
+++ e3_opteller.mjs
@@ -1,14 +1,15 @@
-// Begin van userInput voorbereiden
-import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
-// Einde van userInput voorbereiden
+// // Begin van userInput voorbereiden
+// import * as readline from 'node:readline/promises';
+// import{stdin as input, stdout as output} from 'node:process';
+// const userInput = readline.createInterface({input, output});
+// // Einde van userInput voorbereiden
 
 
 
-let num1=parseFloat(await userInput.question('Geef een getal in:'));
+// let num1=parseFloat(await userInput.question('Geef een getal in:'));
 
-let num2=parseFloat(await userInput.question('Geef een andere  getal in:'));
+// let num2=parseFloat(await userInput.question('Geef een andere  getal in:'));
 
-console.log('je resultaat is '+ (num1 + num2))
-process.exit();
+// console.log('je resultaat is '+ (num1 + num2))
+// process.exit();
+let 
